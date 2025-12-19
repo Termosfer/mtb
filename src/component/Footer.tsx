5d73a42b201e08115d99a486e80e33940accf7b0
Toghrul <ti-togrul@mail.ru>
2025-12-20 00:42:36 +0400

    mail updated

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,8 +2,6 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-10 mt-20" id="footer">
       <div className="max-w-7xl mx-auto px-4 sm:px-8 md:px-16 xl:px-30 flex flex-col md:flex-row justify-between items-center md:items-start gap-8 md:gap-6">
-        
-       
         <div className="flex flex-col gap-5 text-center md:text-left w-full md:w-auto">
           <div className="flex items-center gap-3 md:gap-5 justify-center md:justify-start">
             <svg
@@ -25,7 +23,10 @@ const Footer = () => {
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
               />
             </svg>
-            <p className="text-xs md:text-xs">Bakı, Azərbaycan</p>
+            <p className="text-xs md:text-xs">
+              Bakı, Azərbaycan <br/> Binəqədi rayonu, Rəsulzadə qəsəbəsi,
+              İ.Cümşüdov küçəsi 2F
+            </p>
           </div>
 
           <div className="flex items-center gap-3 md:gap-5 justify-center md:justify-start">
@@ -43,7 +44,7 @@ const Footer = () => {
                 d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
               />
             </svg>
-            <p className="text-xs md:text-xs">+994 XX XXX XX XX</p>
+            <p className="text-xs md:text-xs">+994105553130</p>
           </div>
 
           <div className="flex items-center gap-3 md:gap-5 justify-center md:justify-start">
@@ -61,27 +62,35 @@ const Footer = () => {
                 d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z"
               />
             </svg>
-            <p className="text-xs md:text-xs">info@mtbcorporate.az</p>
+            <p className="text-xs md:text-xs">kuliyevanar@gmail.com</p>
           </div>
         </div>
 
-       
         <div className="flex gap-4 justify-center md:justify-start w-full md:w-auto">
-          <a href="#" className="hover:text-blue-400 transition text-xs md:text-xs">
+          <a
+            href="#"
+            className="hover:text-blue-400 transition text-xs md:text-xs"
+          >
             Facebook
           </a>
-          <a href="#" className="hover:text-blue-400 transition text-xs md:text-xs">
+          <a
+            href="#"
+            className="hover:text-blue-400 transition text-xs md:text-xs"
+          >
             Instagram
           </a>
-          <a href="#" className="hover:text-blue-400 transition text-xs md:text-xs">
+          <a
+            href="#"
+            className="hover:text-blue-400 transition text-xs md:text-xs"
+          >
             LinkedIn
           </a>
         </div>
       </div>
 
-      
       <div className="mt-6 text-center text-gray-400 text-xs md:text-xs">
-        &copy; {new Date().getFullYear()} MTB İncorporate MMC. Bütün hüquqlar qorunur.
+        &copy; {new Date().getFullYear()} MTB İncorporate MMC. Bütün hüquqlar
+        qorunur.
       </div>
     </footer>
   );
