b7ce6eb131272eb862ca929f925da4d1597c264c
Toghrul <ti-togrul@mail.ru>
2025-12-20 06:37:08 +0400

    qwe

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -55,7 +55,7 @@ const Header = () => {
       <img
         src={logo}
         alt="Logo"
-        className="h-15 w-30 sm:h-15   md:w-40  lg:w-40 2xl:w-50  object-cover"
+        className="h-15 w-30 sm:h-15   md:w-40  lg:w-40 2xl:w-50  2xl:h-20 object-cover"
       />
 
       <ul className="hidden md:flex gap-4 lg:gap-6 text-sm lg:text-sm  2xl:text-base font-medium">
