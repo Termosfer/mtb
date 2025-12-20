7d1375a1b0651d02bf45c5ed61faba4de6d74287
Toghrul <ti-togrul@mail.ru>
2025-12-20 06:27:57 +0400

    qwe

--- src/component/ScrollToTop.tsx
+++ src/component/ScrollToTop.tsx
@@ -1,19 +1,26 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 
-const Scrolltotop = () => {
-
- const { pathname } = useLocation();
+const ScrollToTop = () => {
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth", 
-    });
-  }, [pathname]);
+    if (hash) {
+      // hash varsa, elementə scroll
+      const element = document.getElementById(hash.replace("#", ""));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+
+        // URL hash-i silmək
+        window.history.replaceState(null, "", pathname);
+      }
+    } else {
+      // hash yoxdursa, səhifənin başına scroll
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }
+  }, [pathname, hash]);
 
   return null;
 };
 
-export default Scrolltotop;
+export default ScrollToTop;
