bacbaf45293dc68d68096bea2d04c5763284ef09
Toghrul <ti-togrul@mail.ru>
2025-12-21 07:26:07 +0400

    qwe

--- src/component/ScrollToTop.tsx
+++ src/component/ScrollToTop.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+/* import { useEffect, useRef } from "react";
 import { useLocation } from "react-router";
 
 const ScrollToTop = () => {
@@ -22,6 +22,45 @@ const ScrollToTop = () => {
       const y = window.scrollY;
       window.history.replaceState(null, "", pathname);
       window.scrollTo(0, y);
+    }
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
+ */
+
+
+import { useEffect, useRef } from "react";
+import { useLocation } from "react-router";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  const firstLoad = useRef(true);
+
+  useEffect(() => {
+    // Browser scroll restoration manual
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+
+    // İlk load
+    if (firstLoad.current) {
+      firstLoad.current = false;
+      // Refresh zamanı hash varsa, manual scroll
+      if (window.location.hash) {
+        const id = window.location.hash.replace("#", "");
+        const el = document.getElementById(id);
+        if (el) {
+          const HEADER_HEIGHT = 80; // px
+          const y = el.offsetTop - HEADER_HEIGHT;
+          window.history.replaceState(null, "", pathname);
+          window.scrollTo({ top: y, behavior: "auto" });
+        }
+      } else {
+        window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+      }
     }
   }, [pathname]);
 
